65d24d66c9462a8c683de62137eebe25ab1c7597
qinmoM <ma200511@qq.com>
2025-06-23 23:06:50 +0800

    Add README.Improve readability

---
+++ README.md
@@ -0,0 +1,17 @@
+# Pixel-ImageCompression  
+一个简单的图像压缩为像素的算法。  
+  
+## 项目介绍：  
+- 该项目对标我的[EG-Engine2D](https://github.com/qinmoM/EG-Engine2D)项目中的像素化功能。  
+- 用于得到图像的像素数据，使其可以在另一个项目的Model中使用。  
+  
+## 使用方法：  
+- 确保vscode可以正常使用，注意安装C/C++扩展和编译器，并配置好[raylib](https://github.com/raysan5/raylib)的编译环境。  
+- 将项目克隆到本地，在vscode中打开项目文件夹。  
+- 打开main.cpp文件，修改`IMAGE`变量为需要压缩的图像路径，`WIDTH`和`HEIGHT`为一列和一行的像素数量（注意要和原图的比例一致）。  
+- 编译运行程序，终端会输出压缩后的像素数据，并在窗口中显示压缩后的图像。  
+  
+## 版权声明：  
+- 本项目的预览窗口使用[raylib图形库](https://github.com/raysan5/raylib)，其版权归原作者所有。  
+  
+> 欢迎讨论。  
--- main.cpp
+++ main.cpp
@@ -1,46 +1,66 @@
+/*              Pixel-ImageCompression
+ *  The number of the mapping arrary elements should not be too large
+ *  otherwise it will cause performance issues.
+ *  About 20000 pixels sqares cen be drawn in 1 millisecond.
+ * */
+
 #include <iostream>
 #include <raylib.h>
 #include <ctime>
 #include "PixelMappingTable.h"
 
-// The number of the mapping arrary elements should not be too large
-// otherwise it will cause performance issues.
-// About 20000 pixels sqares cen be drawn in 1 millisecond.
+const int WIDTH = 40;                           // the number of pixels in width after compression
+const int HEIGHT = 79;                          // the number of pixels in height after compression
+const char* IMAGE = "res/Pixel_fufu.png";       // the path of the image file
+
+const int SIZE = 10;                            // the size of each pixel in the preview window
+const Color COLOR = { 200, 200, 200, 255 };     // tht color of the background when previewing
 
 int main()
 {
-    Image image = LoadImage("res/Pixel_piao.jpg"); // load image
+    Image image = LoadImage(IMAGE); // load image
     PixelMappingTable table; // initialize mapping table
-    std::vector<std::vector<Color>> array = table.getOriginalColor(image, 37, 52); // original color
+    std::vector<std::vector<Color>> array = table.getOriginalColor(image, WIDTH, HEIGHT); // original color
     std::vector<std::vector<uint8_t>> mapping; // finial mapping array
     mapping = table.getMappingArray(array); // get mapping array
 
-    std::cout << "{ ";
+    std::cout << "{\n";
     for (int i = 0; i < mapping.size(); i++)
     {
         std::cout << "{";
         for (int j = 0; j < mapping[i].size(); j++)
         {
-            std::cout << static_cast<int>(mapping[i][j]) << ", ";
+            std::cout << static_cast<int>(mapping[i][j]);
+            if (mapping[i].size() - 1 != j)
+            {
+                std::cout << ", ";
+            }
         }
-        std::cout << "},\n";
+        if (mapping.size() - 1 == i)
+        {
+            std::cout << "}\n";
+        }
+        else
+        {
+            std::cout << "},\n";
+        }
     }
     std::cout << "}\n";
 
-    InitWindow(849, 1200, "Pixel Color Mapping Table"); // initialize window
-    int size = 4;
+    InitWindow(WIDTH * SIZE, HEIGHT * SIZE, "Preview"); // initialize window
+
     SetTargetFPS(60); // set FPS
     while (!WindowShouldClose()) // main loop
     {
         BeginDrawing(); // begin drawing
-        ClearBackground(WHITE); // clear background
+        ClearBackground(COLOR); // clear background
 
         // std::cout << clock() << " ";
         for (int i = 0; i < mapping.size(); i++)
         {
             for (int j = 0; j < mapping[i].size(); j++)
             {
-                DrawRectangle(j * size, i * size, size, size, table.colorPalette[mapping[i][j]]);
+                DrawRectangle(j * SIZE, i * SIZE, SIZE, SIZE, table.colorPalette[mapping[i][j]]);
             }
         }
         // std::cout << clock() << "\n";
